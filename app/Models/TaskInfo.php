e232fbc16a7bb680839994329af4d4bfa3de08a4
d0t-hck <42749949+d0t-hck@users.noreply.github.com>
2022-04-12 20:43:19 +0600

    models

---
+++ app/Models/TaskInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskInfo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'time_start',
+        'time_end',
+        'comment',
+        'job_id'
+    ];
+}
