a85d9b674c8d51ebcd28cd5cb578fd731b3acfd0
Gulzada98 <akmsral2002@gmail.com>
2022-04-18 21:20:27 +0600

    html to blade

--- resources/views/ManagerPersonalStatistics.blade.php
+++ resources/views/ManagerPersonalStatistics.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
     <title>Manager Personal Statistics</title>
 </head>
 
--- resources/views/PersonalTasksPage.blade.php
+++ resources/views/PersonalTasksPage.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
     <title>Document</title>
 </head>
 
@@ -15,7 +15,7 @@
         <div class="divWrapper">
             <div class="textWrapper displaySettings">
                 <div>
-                    <img class="userImg" src="../img/user_2.png">
+                <img class="userImg" src="{{ asset('img/user_2.png') }}" />
                     <input value="Имя пользователя" disabled class="form-control">
                 </div>
                 <h1 class="titleH1">Учет рабочего времени</h1>
